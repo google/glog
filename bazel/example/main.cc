ad0b4ec4b0966b8ff16b94b28947f38e6b2155f9
Rodrigo Queiro <rodrigoq@google.com>
2018-01-31 14:00:25 +0100

    Allow using glog/stl_logging.h with Bazel
    Fixes #289.

--- bazel/example/main.cc
+++ bazel/example/main.cc
@@ -1,5 +1,6 @@
 #include <gflags/gflags.h>
 #include <glog/logging.h>
+#include <glog/stl_logging.h>
 
 int main(int argc, char* argv[]) {
   // Initialize Google's logging library.
@@ -9,6 +10,10 @@ int main(int argc, char* argv[]) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
   LOG(INFO) << "Hello, world!";
+
+  // glog/stl_logging.h allows logging STL containers.
+  std::vector<int> x {1, 2, 3};
+  LOG(INFO) << "ABC, it's easy as " << x;
 
   return 0;
 }
