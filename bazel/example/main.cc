55cc27b6eca3d7906fc1a920ca95df7717deb4e7
Rodrigo Queiro <rodrigoq@google.com>
2018-02-01 09:36:50 +0100

    Bazel: make example more portable

--- bazel/example/main.cc
+++ bazel/example/main.cc
@@ -12,7 +12,10 @@ int main(int argc, char* argv[]) {
   LOG(INFO) << "Hello, world!";
 
   // glog/stl_logging.h allows logging STL containers.
-  std::vector<int> x {1, 2, 3};
+  std::vector<int> x;
+  x.push_back(1);
+  x.push_back(2);
+  x.push_back(3);
   LOG(INFO) << "ABC, it's easy as " << x;
 
   return 0;
