167b151e4286f23a8b54999fc75db20803eefd16
Juho Eskeli <juho.eskeli@timegate.com>
2019-10-17 13:49:47 +0300

    Compiler flags to reduce library size

--- toolchains/armr5-none-eabi.cmake
+++ toolchains/armr5-none-eabi.cmake
@@ -11,9 +11,9 @@ SET(CMAKE_SYSTEM_VERSION 1)
 # set compiler
 set(CMAKE_C_COMPILER armr5-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER armr5-none-eabi-g++)
-set(CMAKE_C_FLAGS "-mcpu=cortex-r5 -mfloat-abi=hard  -mfpu=vfpv3-d16 -specs=nosys.specs")
-set(CMAKE_CXX_FLAGS "-mcpu=cortex-r5 -mfloat-abi=hard  -mfpu=vfpv3-d16 -specs=nosys.specs")
-set(CMAKE_EXE_LINKER_FLAGS "-specs=nosys.specs")
+set(CMAKE_C_FLAGS "-mcpu=cortex-r5 -mfloat-abi=hard  -mfpu=vfpv3-d16 -specs=nosys.specs -ffunction-sections -fdata-sections")
+set(CMAKE_CXX_FLAGS "-mcpu=cortex-r5 -mfloat-abi=hard  -mfpu=vfpv3-d16 -specs=nosys.specs -fno-exceptions -ffunction-sections -fdata-sections")
+set(CMAKE_EXE_LINKER_FLAGS "-specs=nosys.specs --gc-sections")
 
 # set c++ standard
 set(CMAKE_CXX_STANDARD 11)
