355ebf91edb102ec164c1a413f69d1c21f8af515
Juho Eskeli <juho.eskeli@timegate.com>
2019-10-17 11:30:21 +0300

    Update compiler flags

--- toolchains/armr5-none-eabi.cmake
+++ toolchains/armr5-none-eabi.cmake
@@ -11,8 +11,8 @@ SET(CMAKE_SYSTEM_VERSION 1)
 # set compiler
 set(CMAKE_C_COMPILER armr5-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER armr5-none-eabi-g++)
-set(CMAKE_C_FLAGS "-mfloat-abi=soft -mcpu=cortex-r5 -specs=nosys.specs")
-set(CMAKE_CXX_FLAGS "-mfloat-abi=soft -mcpu=cortex-r5 -specs=nosys.specs")
+set(CMAKE_C_FLAGS "-mcpu=cortex-r5 -mfloat-abi=hard  -mfpu=vfpv3-d16 -specs=nosys.specs")
+set(CMAKE_CXX_FLAGS "-mcpu=cortex-r5 -mfloat-abi=hard  -mfpu=vfpv3-d16 -specs=nosys.specs")
 set(CMAKE_EXE_LINKER_FLAGS "-specs=nosys.specs")
 
 # set c++ standard
