0bd1584f9330247e48037902251d5adfa4e2f919
Juho Eskeli <juho.eskeli@timegate.com>
2019-10-17 14:01:38 +0300

    disable rtti in compilation

--- toolchains/armr5-none-eabi.cmake
+++ toolchains/armr5-none-eabi.cmake
@@ -12,7 +12,7 @@ SET(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_C_COMPILER armr5-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER armr5-none-eabi-g++)
 set(CMAKE_C_FLAGS "-mcpu=cortex-r5 -mfloat-abi=hard  -mfpu=vfpv3-d16 -specs=nosys.specs -ffunction-sections -fdata-sections")
-set(CMAKE_CXX_FLAGS "-mcpu=cortex-r5 -mfloat-abi=hard  -mfpu=vfpv3-d16 -specs=nosys.specs -fno-exceptions -ffunction-sections -fdata-sections")
+set(CMAKE_CXX_FLAGS "-mcpu=cortex-r5 -mfloat-abi=hard  -mfpu=vfpv3-d16 -fno-rtti -specs=nosys.specs -fno-exceptions -ffunction-sections -fdata-sections")
 set(CMAKE_EXE_LINKER_FLAGS "-specs=nosys.specs --gc-sections")
 
 # set c++ standard
