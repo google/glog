2df614711b97899bd1a1326518ff76297a804173
pkubaj <pkubaj@users.noreply.github.com>
2021-05-28 16:37:39 +0200

    Fix unwind detection on FreeBSD/powerpc64

--- cmake/FindUnwind.cmake
+++ cmake/FindUnwind.cmake
@@ -19,7 +19,8 @@ elseif (CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64" OR
     set (Unwind_ARCH "x86_64")
 elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "^i.86$")
     set (Unwind_ARCH "x86")
-elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "^ppc64")
+elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "^ppc64" OR
+        CMAKE_SYSTEM_PROCESSOR MATCHES "^powerpc64")
     set (Unwind_ARCH "ppc64")
 elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "^ppc")
     set (Unwind_ARCH "ppc32")
