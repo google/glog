7441caab1ba68f3d63db229b6a3db7d9674b4120
huangqinjin <huangqinjin@gmail.com>
2021-09-04 16:05:33 +0800

    Remove duplicated logcat output

--- src/logging.cc
+++ src/logging.cc
@@ -1682,23 +1682,6 @@ ostream& LogMessage::stream() {
   return data_->stream_;
 }
 
-namespace {
-#if defined(__ANDROID__)
-int AndroidLogLevel(const int severity) {
-  switch (severity) {
-    case 3:
-      return ANDROID_LOG_FATAL;
-    case 2:
-      return ANDROID_LOG_ERROR;
-    case 1:
-      return ANDROID_LOG_WARN;
-    default:
-      return ANDROID_LOG_INFO;
-  }
-}
-#endif  // defined(__ANDROID__)
-}  // namespace
-
 // Flush buffered message, called by the destructor, or any other function
 // that needs to synchronize the log.
 void LogMessage::Flush() {
@@ -1733,12 +1716,6 @@ void LogMessage::Flush() {
     ++num_messages_[static_cast<int>(data_->severity_)];
   }
   LogDestination::WaitForSinks(data_);
-
-#if defined(__ANDROID__)
-  const int level = AndroidLogLevel((int)data_->severity_);
-  const std::string text = std::string(data_->message_text_);
-  __android_log_write(level, "native", text.substr(0,data_->num_chars_to_log_).c_str());
-#endif  // defined(__ANDROID__)
 
   if (append_newline) {
     // Fix the ostrstream back how it was before we screwed with it.
