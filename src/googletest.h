e567cfc44212aa946c5c1740911aefe5aad882e1
Sergiu Deitsch <sergiud@users.noreply.github.com>
2023-10-06 00:34:38 +0200

    fix: address sanitizer failures (#956)

--- src/googletest.h
+++ src/googletest.h
@@ -45,6 +45,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <map>
+#include <new>
 #include <sstream>
 #include <string>
 #include <utility>
@@ -637,11 +638,19 @@ void (*g_new_hook)() = nullptr;
 
 _END_GOOGLE_NAMESPACE_
 
-void* operator new(size_t size) GOOGLE_GLOG_THROW_BAD_ALLOC {
+void* operator new(size_t size, const std::nothrow_t&) noexcept {
   if (GOOGLE_NAMESPACE::g_new_hook) {
     GOOGLE_NAMESPACE::g_new_hook();
   }
   return malloc(size);
+}
+
+void* operator new(size_t size) GOOGLE_GLOG_THROW_BAD_ALLOC {
+  void* p = ::operator new(size, std::nothrow);
+  if (p == nullptr) {
+    throw std::bad_alloc{};
+  }
+  return p;
 }
 
 void* operator new[](size_t size) GOOGLE_GLOG_THROW_BAD_ALLOC {
