4a40e5e318cb72dbd74a41df3ad7d8b016377206
Hari Krishna Sunder <hari90@users.noreply.github.com>
2024-06-24 11:32:25 -0700

    Update docs

--- src/glog/vlog_is_on.h
+++ src/glog/vlog_is_on.h
@@ -103,10 +103,9 @@ namespace google {
 // Set VLOG(_IS_ON) level for module_pattern to log_level.
 // This lets us dynamically control what is normally set by the --vmodule flag.
 // Returns the level that previously applied to module_pattern.
-// NOTE: To change the log level for VLOG(_IS_ON) sites that matched a prior
-// --vmodule pattern the exact pattern has to be supplied again. (If no
-// --vmodule pattern applied to them before then the first matching pattern will
-// start to control them.)
+// NOTE: All VLOG(_IS_ON) sites that have not matched any prior module_patterns
+// will be re-evaluated with the provided module_pattern. Sites that have
+// already matched a different module_pattern will not be affected.
 extern GLOG_EXPORT int SetVLOGLevel(const char* module_pattern, int log_level);
 
 // Various declarations needed for VLOG_IS_ON above: =========================
